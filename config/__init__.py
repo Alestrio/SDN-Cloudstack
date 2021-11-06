a5633b1f02d97088586a920026edf8e93dc56f15
Alexis LEBEL <lebalexis@gmail.com>
2021-11-06 18:28:18 +0100

    [GENERAL] Added copyright

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+#  SDN-Cloudstack - API
+#  Third semester project, Technical Degree, Networks and Telecommunications
+#  Copyright (c) 2021-2022
+#  Alexis LEBEL, Elwan LEFEVRE, Laurent HUSSENET
+#  This code belongs exclusively to its authors, use, redistribution or
+#  reproduction forbidden except with authorization from the authors.
+#
+
--- snmp/snmp_bulk.py
+++ snmp/snmp_bulk.py
@@ -1,3 +1,11 @@
+#  SDN-Cloudstack - API
+#  Third semester project, Technical Degree, Networks and Telecommunications
+#  Copyright (c) 2021-2022
+#  Alexis LEBEL, Elwan LEFEVRE, Laurent HUSSENET
+#  This code belongs exclusively to its authors, use, redistribution or
+#  reproduction forbidden except with authorization from the authors.
+#
+
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 
