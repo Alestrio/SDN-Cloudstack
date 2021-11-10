75b6c275b8692688317b18f3b9de7d0638a4556b
Alexis <lebalexis@gmail.com>
2021-11-10 22:28:11 +0100

    [API] renamed file (conflict package)

--- api/api_run.py
+++ api/api_run.py
@@ -4,49 +4,45 @@
 #  Alexis LEBEL, Elwan LEFEVRE, Laurent HUSSENET
 #  This code belongs exclusively to its authors, use, redistribution or
 #  reproduction forbidden except with authorization from the authors.
-#
-#
-#
-#
+
 
 import sys
 
 import yaml
 
 from models import *
-from api.snmp import modules
+from snmp import modules
 from db import database
 from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
-db = database.Database()
 try:
-    with open('../config/config.yaml') as file:
-        config = yaml.load(file, Loader=yaml.Loader)['api']
-        ROUTE_PREFIX = config['route_prefix']
-except:
+    file = open('../config/config.yaml')
+    config = yaml.load(file, Loader=yaml.Loader)
+    ROUTE_PREFIX = config['api']['route_prefix']
+    IP_SWITCH = config['api']['ip_switch']
+    db = database.Database(config['database'])
+    OIDS = config['oids']
+except FileNotFoundError:
     print('No config file (ERRNO 101)')
-
-
-ip_switch = str(sys.argv[1])
 
 
 @api.get(f"{ROUTE_PREFIX}/vlans")
 def get_vlans():
-    response = modules.get_all_vlans(ip_switch)
+    response = modules.get_all_vlans(IP_SWITCH)
     return response
 
 
 @api.get(f"{ROUTE_PREFIX}/interfaces")
 def get_interfaces():
-    response = modules.get_all_interfaces(ip_switch)
+    response = modules.get_all_interfaces(IP_SWITCH)
     return response
 
 
 @api.get(ROUTE_PREFIX+"/vlans/{vl_id}")
 def get_vlan_id(vl_id: int):
-    if modules.check_if_id_exists(ip_switch, vl_id, '1.3.6.1.4.1.9.9.46.1.3.1.1.4'):
-        response = modules.get_vlan_by_id(ip_switch, vl_id)
+    if modules.check_if_id_exists(IP_SWITCH, vl_id, OIDS['vlans']['name']):
+        response = modules.get_vlan_by_id(IP_SWITCH, vl_id)
     else:
         raise HTTPException(status_code=404, detail="Vlan not found")
     return response
@@ -54,8 +50,8 @@ def get_vlan_id(vl_id: int):
 
 @api.get(ROUTE_PREFIX+"/interfaces/{if_id}")
 def get_if_id(if_id: int):
-    if modules.check_if_id_exists(ip_switch, if_id, '1.3.6.1.2.1.2.2.1.2'):
-        response = modules.get_interface_by_id(ip_switch, if_id)
+    if modules.check_if_id_exists(IP_SWITCH, if_id, OIDS['interfaces']['description']):
+        response = modules.get_interface_by_id(IP_SWITCH, if_id)
     else:
         raise HTTPException(status_code=404, detail="Interface not found")
     return response
@@ -64,7 +60,7 @@ def get_if_id(if_id: int):
 @api.post(ROUTE_PREFIX+"/interfaces/{if_id}")
 def set_vlan_on_interface(if_id: int, body: VlanId):
     if body:
-        if modules.set_interface_vlan(ip_switch, if_id, body.vlan_id):
+        if modules.set_interface_vlan(IP_SWITCH, if_id, body.vlan_id):
             return {"message": "Vlan successfully changed"}
         else:
             raise HTTPException(status_code=404, detail="Interface or Vlan not found")
@@ -72,7 +68,7 @@ def set_vlan_on_interface(if_id: int, body: VlanId):
 
 @api.get(f"{ROUTE_PREFIX}/neighbors")
 def get_cdp_neighbors():
-    response = modules.get_cdp_neighbors(ip_switch)
+    response = modules.get_cdp_neighbors(IP_SWITCH)
     return response
 
 
