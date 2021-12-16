078cbab2d80070b8734a121c9f8e45d0eb8012fe
Elwan LEFEVRE <elwanlefevre.51@gmail.com>
2021-12-16 08:06:35 +0100

    [CLI-CONFIGURATOR] Initial commit

---
+++ src/cli_configurator.py
@@ -0,0 +1,27 @@
+#  SDN-Cloudstack - CLI-CONFIGURATION
+#  Third semester project, Technical Degree, Networks and Telecommunications
+#  Copyright (c) 2021-2022
+#  Alexis LEBEL, Elwan LEFEVRE, Laurent HUSSENET
+#  This code belongs exclusively to its authors, use, redistribution or
+#  reproduction forbidden except with authorization from the authors.
+
+import yaml
+import json
+
+class Cli_configurator:
+    """This class will generate for a json or yaml files the cli(Cisco) needed for the configuration"""
+    def get_data(self,path):
+        # This function get the data from path
+        pass
+
+    def set_interface(self):
+        # This function will create the config for all interface from self.interface and return str(int_config)
+        pass
+
+    def set_vlan(self):
+        # This function will create the config for all vlan from self.vlan and return str(vlan_config)
+        pass
+
+    def set_trunk(self):
+        # This function will
+        pass
