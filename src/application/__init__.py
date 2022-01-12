f4f32743e29fadbb13a501925a888ff00e9db818
Elwan-51 <elwan.lefevre@etudiant.univ-reims.fr>
2022-01-12 12:01:43 +0100

    [APPLICATION] Re-organised the working directory

--- src/application/__init__.py
+++
@@ -1,6 +0,0 @@
-#  SDN-Cloudstack - APPLICATION
-#  Third semester project, Technical Degree, Networks and Telecommunications
-#  Copyright (c) 2021-2022
-#  Alexis LEBEL, Elwan LEFEVRE, Laurent HUSSENET
-#  This code belongs exclusively to its authors, use, redistribution or
-#  reproduction forbidden except with authorization from the authors.
